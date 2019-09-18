5c7a365b6270ab67a00afd1ed799698b3bb2aef7
chathura <jrcmadushanka@gmail.com>
2019-09-19 02:11:04 +0530

    File Uploader API

--- src/main/java/com/example/demo/ComplexItApplication.java
+++ src/main/java/com/example/demo/ComplexItApplication.java
@@ -2,8 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.example.demo.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
+@EnableJpaAuditing
 public class ComplexItApplication {
 
 	public static void main(String[] args) {
